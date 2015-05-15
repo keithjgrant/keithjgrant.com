66937dd001edf9f201439ed993f9f8958b0a617c
Keith Grant <keith.grant@theice.com>
2015-05-15 12:36:38 -0400

    gulp clean, copy:images tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,17 +3,26 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var sass = require('gulp-sass');
+var del = require('del');
 var minifyCSS = require('gulp-minify-css');
 var jadeVars = require('./src/jade-vars.json');
 
-gulp.task('jade', function() {
+gulp.task('clean', function (done) {
+  del('./build/**/*', done);
+});
+
+gulp.task('copy:images', function () {
+  gulp.src(['./src/images/**/*'])
+    .pipe(gulp.dest('build/images'));
+});
+
+gulp.task('jade', function () {
   gulp.src('./src/jade/**/*.jade')
     .pipe(jade({
       locals: jadeVars
     }))
     .pipe(gulp.dest('./build'))
 });
-
 
 gulp.task('sass', function () {
   gulp.src('./src/sass/main.scss')
@@ -22,7 +31,8 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('build', ['jade', 'sass']);
+gulp.task('build', ['clean', 'copy:images', 'jade', 'sass']);
+
 gulp.task('watch', function () {
   gulp.watch('./src/**/*', ['build']);
 });
