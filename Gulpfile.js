7c8b415705d45579671cf64fc2ecc7fc83ff9a37
Keith Grant <keith.grant@theice.com>
2015-05-15 13:00:50 -0400

    gulp server and livereload

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 var sass = require('gulp-sass');
+var connect = require('gulp-connect');
 var del = require('del');
 var minifyCSS = require('gulp-minify-css');
 var jadeVars = require('./src/jade-vars.json');
@@ -22,19 +23,35 @@ gulp.task('jade', function () {
       locals: jadeVars
     }))
     .pipe(gulp.dest('./build'))
+    .pipe(connect.reload());
 });
 
 gulp.task('sass', function () {
   gulp.src('./src/sass/main.scss')
     .pipe(sass().on('error', sass.logError))
-    // .pipe(minifyCSS())
-    .pipe(gulp.dest('./build/css'));
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(connect.reload());
+});
+gulp.task('sass:verbose', function () {
+  gulp.src('./src/sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(connect.reload());
 });
 
-gulp.task('build', ['clean', 'copy:images', 'jade', 'sass']);
+gulp.task('connect', function () {
+  connect.server({
+    root: ['build'],
+    port: 4242,
+    livereload: true
+  });
+});
 
-gulp.task('watch', function () {
+gulp.task('watch', ['connect'], function () {
   gulp.watch('./src/**/*', ['build']);
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('build', ['copy:images', 'jade', 'sass:verbose']);
+
+gulp.task('default', ['build', 'connect', 'watch']);
