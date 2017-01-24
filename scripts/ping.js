73bdee97dfef07157214191caf4dbfe43879a397
Keith J. Grant <keithjgrant@gmail.com>
2017-01-24 10:55:56 -0500

    superfeedr integration

---
+++ scripts/ping.js
@@ -0,0 +1,16 @@
+var request = require('request');
+
+request.post(
+  'http://keithjgrant.superfeedr.com/',
+  {
+    json: {
+      "hub.mode": "publish",
+      "hub.url": "http://keithjgrant.com/posts/index.xml",
+    }
+  },
+  function (error, response, body) {
+    if (!error && response.statusCode == 204) {
+      console.log("SuperFeedr ping successful")
+    }
+  }
+);
