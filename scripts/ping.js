b21431f4d83b9adc83b6a57526d866b9382a2690
Keith J. Grant <keithjgrant@gmail.com>
2017-01-24 22:06:02 -0500

    rebuild

--- scripts/ping.js
+++ scripts/ping.js
@@ -1,7 +1,7 @@
 var request = require('request');
 
 request.post(
-  'http://keithjgrant.superfeedr.com/?hub.mode=publish&hub.url[]=http://keithjgrant.com&hub.url=http://keithjgrant.com/posts',
+  'http://keithjgrant.superfeedr.com/?hub.mode=publish&hub.url[]=http://keithjgrant.com&hub.url=http://keithjgrant.com/note',
   function (error, response, body) {
     if (!error && response.statusCode == 204) {
       console.log("SuperFeedr ping successful")
