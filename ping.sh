2b9933d546ced0fcbb202ea3bc21ca8e98198526
Keith J. Grant <me@keithjgrant.com>
2018-01-11 14:35:44 -0500

    fix self url

--- ping.sh
+++ ping.sh
@@ -1,1 +1,1 @@
-curl --data "hub.mode=publish&hub.url[]=https%3A%2F%2Fkeithjgrant.com&hub.url[]=https%3A%2F%2Fkeithjgrant.com%2Fposts%2F" http://keithjgrant.superfeedr.com
+curl --data "hub.mode=publish&hub.url[]=https%3A%2F%2Fkeithjgrant.com%2Findex.xml&hub.url[]=https%3A%2F%2Fkeithjgrant.com%2Fposts%2Findex.xml" http://keithjgrant.superfeedr.com
